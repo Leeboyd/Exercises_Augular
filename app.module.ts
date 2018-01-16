44f49411e930899f36649479643e89bcd184bd9c
LeeBoy <jobboy19890101@gmail.com>
2018-01-16 14:10:09 +0800

    two-way binding

--- app.module.ts
+++ app.module.ts
@@ -1,23 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { StockComponent } from './stockModule/stock.component'
+import { BindingComponent } from './bindingModule/binding.component'
 
 // Directive
 import { HighlightDirective } from './Directive/highlight.directive'
 
 // Service
-import { StockService } from './Service/stock.service'
+import { StockService } from './Service/stock.service';
+
+// Pipe
+import { DateFormatterPipe } from './Pipe/date-formatter.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     StockComponent,
-    HighlightDirective
+    BindingComponent,
+    HighlightDirective,
+    DateFormatterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [
     StockService
---
+++ bindingModule/binding.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'formFName',
+  templateUrl: './binding.component.html',
+  // styleUrls: ['./stock.component.css']
+})
+
+export class BindingComponent {
+  firstName = 'jon'
+}
