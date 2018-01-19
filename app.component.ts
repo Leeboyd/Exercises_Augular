b6469d531aac3a8a9ee8c8d4c6fea5f04ef7036b
LeeBoy <jobboy19890101@gmail.com>
2018-01-19 12:15:43 +0800

    decorater

--- app.component.ts
+++ app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+export function ReadOnly(defaultValue) {
+  return (target, key: string) => {
+      target[key] = defaultValue;
+      Object.defineProperty(target, key, { writable: false });
+  }
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hello MABOW';
+  @ReadOnly('MABOW GATE')
+  comment: string
+  title: string
+  constructor() { }
+  change (msg: string) {
+    console.log(msg)
+    this.comment = 'NOTEST'
+  }
 }
